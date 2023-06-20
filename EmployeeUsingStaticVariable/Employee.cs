d4b9314d3fa330376f7926ce686df3fdd222db28
SnehalS26 <Snehalshirsath26@gmail.com>
2023-06-20 17:29:05 +0530

    Add project files.

---
+++ EmployeeUsingStaticVariable/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Create a class Employee
+Accept name & basic salary from the constructor
+Class must auto generate the emp id using static
+
+Create method CalculateSalary() for salary calculation
+Hra 40 % of bs
+Ta  20 % of bs
+Da  15 % of bs
+Pf  12 % of bs
+
+Calculate the total salary & Gross salary of employee
+
+Create Print method to display employee details
+Create static method to display total count of employee
+*/
+namespace EmployeeUsingStaticVariable
+{
+    internal class Employee
+    {
+        private int empid;
+        private string name;
+        private double basicSal, totalSal, grossSal, hra, ta, da, pf;
+        private static int count;
+
+        public Employee(string name, double basicSal)
+        {
+            count++;
+            empid = count;
+            this.name = name;
+            this.basicSal = basicSal;
+        }
+
+        public static int GetCount()
+        {
+            return count;
+        }
+
+        public void CalculateSalary()
+        {
+            hra = basicSal * 0.40;
+            ta = basicSal * 0.20;
+            da = basicSal * 0.15;
+            pf = basicSal * 0.12;
+
+            totalSal = hra + ta + da;
+
+            grossSal = (hra + ta + da) - pf;
+        }
+
+
+        public string PrintDeatils()
+        {
+            return $"Emp id = {empid} \nEmp Name = {name} \nBasic Salary = {basicSal} \nTotal Salary = {totalSal} \nGross Salary = {grossSal}";
+        }
+    }
+}
---
+++ EmployeeUsingStaticVariable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeUsingStaticVariable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Snehal", 10000);
+            emp1.CalculateSalary();
+
+            Employee emp2 = new Employee("Sneha", 20000);
+            emp2.CalculateSalary();
+
+            //instance Method
+            Console.WriteLine(emp1.PrintDeatils());
+            Console.WriteLine(emp2.PrintDeatils());
+
+            Console.WriteLine(Employee.GetCount());  //print class method
+        }
+    }
+}
